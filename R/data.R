a08bbd9a8653814b9111916ea70272a4b04708e4
annie <cooperla.90@gmail.com>
2019-08-30 12:41:39 -0400

    start vignette

--- R/data.R
+++ R/data.R
@@ -10,10 +10,13 @@
 #' the mean. The image was created using Google Earth
 #' Engine in August 2018.
 #'
-#' @format A raster image with 371 x 371 pixels, with the WGS84
-#'   projection at ~30 meter resolution.
+#' @format A raster image with 371 x 371 pixels
 #' \describe{
-#'   \item{value}{NDVI, range = 0-1, bounds = -123, -122.9, 43.0002, 43.1}
+#'   \item{range}{0 -- 1}
+#'   \item{bounds}{-123, -122.9, 43.0002, 43.1}
+#'   \item{resolution}{30m x 30m}
+#'   \item{projection}{WGS84}
+#'   \item{scalar}{1}
 #'   ...
 #' }
 "orforest"
@@ -34,11 +37,33 @@
 #' the mean. The image was created using Google Earth
 #' Engine in August 2018.
 #'
-#' @format A raster image with 371 x 371 pixels, with the WGS84
-#'   projection at ~30 meter resolution.
+#' @format A raster image with 371 x 371 pixels
 #' \describe{
-#'   \item{value}{NDVI errors, range = -0.5854638-0.1585918,
-#'     bounds = -123, -122.9, 43.0002, 43.1}
+#'   \item{range}{-0.5854638 -- 0.1585918}
+#'   \item{bounds}{-123, -122.9, 43.0002, 43.1}
+#'   \item{resolution}{30m x 30m}
+#'   \item{projection}{WGS84}
+#'   \item{scalar}{1}
 #'   ...
 #' }
 "normforest"
+
+#' Raster of Enhanced Vegetation Index (EVI) over Oregon, USA.
+#'
+#' A raster image of maximum growing season (May-September)
+#' EVI over Oregon state, USA derived from MODIS 16-day EVI data.
+#' The growing season summary was completed in Google Earth Engine
+#' on 30 August, 2019. Data were filtered using the general quality assurance codes
+#' included with the MODIS data; only the highest quality (0: VI
+#' produced with good quality) data were included.
+#'
+#' @format A raster image with 1918 x 3670 pixels.
+#' \describe{
+#'   \item{range}{0 -- 10000}
+#'   \item{bounds}{-124.7041, -116.4621, 41.99175, 46.29917}
+#'   \item{resolution}{250m x 250m}
+#'   \item{projection}{WGS84}
+#'   \item{scalar}{0.0001}
+#'   ...
+#' }
+"oregonEVI"
